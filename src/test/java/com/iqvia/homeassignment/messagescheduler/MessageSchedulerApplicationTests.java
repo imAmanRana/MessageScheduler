1c0261d1fbf6c436ea4229376b67a41bd6c9ee5b
Amandeep Singh <amandeep_rana@ymail.com>
2020-07-19 09:57:54 -0400

    Initial Project Setup

---
+++ src/test/java/com/iqvia/homeassignment/messagescheduler/MessageSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iqvia.homeassignment.messagescheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
